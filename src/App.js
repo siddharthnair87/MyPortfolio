541dc46c9884592481bb7bb8c61b45e3d25fed2c
Sid Nair <siddharthnair87@outlook.com>
2021-12-14 16:23:11 +0000

    changed Switch to Routes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Home from "./components/pages/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Projects from "./components/pages/Projects";
 import Tools from "./components/pages/Tools";
-import SignUp from "./components/pages/SignUp";
+// import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/tools" component={Tools} />
-          <Route path="/sign-up" component={SignUp} />
-        </Switch>
+        <Routes>
+          <Route path="/" exact component={<Home />} />
+          <Route path="/projects" component={<Projects />} />
+          <Route path="/tools" component={<Tools />} />
+        </Routes>
       </Router>
     </>
   );
